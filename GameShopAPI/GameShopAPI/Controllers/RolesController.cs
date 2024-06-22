a49cbade329907cdb34d3a3b3435d4f50d94da68
m1toi <147705211+m1toi@users.noreply.github.com>
2024-06-23 00:56:06 +0300

    create user and role  controllers

---
+++ GameShopAPI/GameShopAPI/Controllers/RolesController.cs
@@ -0,0 +1,65 @@
+﻿using GameShop.BusinessLogic.Services;
+using GameShop.EntityLayer.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/Roles")]
+    public class RolesController : ControllerBase
+    {
+        private readonly IRoleService _roleService;
+
+        public RolesController(IRoleService roleService)
+        {
+            _roleService = roleService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<List<RoleResponseDto>> GetRoles()
+        {
+            return Ok(_roleService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<RoleResponseDto> GetRole([FromRoute] int id)
+        {
+            return Ok(_roleService.Get(id));           
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult CreateRole([FromBody] RoleRequestDto role)
+        {
+            _roleService.Create(role);
+            return Created("Role created", role);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        
+        public IActionResult UpdateRole([FromRoute] int id, [FromBody] RoleRequestDto updatedRole)
+        {
+            _roleService.Update(id, updatedRole);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public IActionResult DeleteRole([FromRoute] int id)
+        {
+            _roleService.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ GameShopAPI/GameShopAPI/Controllers/UserController.cs
@@ -0,0 +1,80 @@
+﻿using GameShop.BusinessLogic.Services;
+using GameShop.EntityLayer.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Route("api/Users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("/register")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public IActionResult Register([FromBody] UserRequestDto user)
+        {
+            _userService.Register(user);
+            return Created("User created", user);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("/login")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+        public ActionResult<string> Login([FromBody] LoginRequestDto login)
+        {
+             string token = _userService.Login(login);
+             return Ok(token);
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+
+        public ActionResult<List<UserResponseDto>> GetAll()
+        {
+            return Ok(_userService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<UserResponseDto> GetById([FromRoute] int id)
+        {
+            return Ok(_userService.Get(id));
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public IActionResult Update([FromRoute] int id, [FromBody] UserRequestDto updatedUser)
+        {
+            _userService.Update(id, updatedUser);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+
+        public IActionResult Delete([FromRoute] int id)
+        {
+            _userService.Delete(id);
+            return NoContent();
+        }
+    }
+}
