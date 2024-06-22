74311736e8d3862b00b07fb812b5c397497f175a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 20:22:30 +0300

    Create IRoleService.cs

---
+++ GameShopAPI/GameShop.BusinessLogic/Services/RoleService/IRoleService.cs
@@ -0,0 +1,11 @@
+﻿using GameShop.EntityLayer.Dtos;
+using GameShop.EntityLayer.Entities;
+
+namespace GameShop.BusinessLogic.Services
+{
+    public interface IRoleService
+    {
+        List<RoleResponseDto> GetAll();
+        void Create(RoleRequestDto roleDto);
+    }
+}
