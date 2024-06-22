6cc2039f13947a3058f027f958f2b63b2b6c4d54
m1toi <147705211+m1toi@users.noreply.github.com>
2024-06-22 19:13:46 +0300

    create categories and gamecompanies controllers

---
+++ GameShopAPI/GameShopAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using GameShop.BusinessLogic.Services;
+using GameShop.EntityLayer.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.API.Controllers
+{
+    [Route("api/Categories")]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public List<CategoryResponseDto> GetAll()
+        {
+            return _categoryService.GetAll();
+        }
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public CategoryResponseDto GetById([FromRoute] int id)
+        {
+            return _categoryService.GetById(id);
+        }
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public void Create([FromBody] CategoryRequestDto categoryDto)
+        {
+            _categoryService.Create(categoryDto);
+        }
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public void Update([FromRoute] int id, [FromBody] CategoryRequestDto updatedCategoryDto)
+        {
+            _categoryService.Update(id, updatedCategoryDto);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public void Delete([FromRoute] int id)
+        {
+            _categoryService.Delete(id);
+        }
+    }
+}
---
+++ GameShopAPI/GameShopAPI/Controllers/GameCompaniesController.cs
@@ -0,0 +1,58 @@
+﻿using GameShop.BusinessLogic.Services;
+using GameShop.EntityLayer.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.API.Controllers
+{
+    [Route("api/GameCompanies")]
+    public class GameCompaniesController : ControllerBase
+    {
+        private readonly IGameCompanyService _gameCompanyService;
+
+        public GameCompaniesController(IGameCompanyService gameCompanyService)
+        {
+            _gameCompanyService = gameCompanyService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public List<GameCompanyResponseDto> GetAll()
+        {
+            return _gameCompanyService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public GameCompanyResponseDto GetById([FromRoute] int id)
+        {
+            return _gameCompanyService.GetById(id);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public void Create([FromBody] GameCompanyRequestDto gameCompanyDto)
+        {
+            _gameCompanyService.Create(gameCompanyDto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public void Update([FromRoute] int id, [FromBody] GameCompanyRequestDto updatedGameCompanyDto)
+        {
+            _gameCompanyService.Update(id, updatedGameCompanyDto);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        public void Delete([FromRoute] int id)
+        {
+            _gameCompanyService.Delete(id);
+        }   
+    }
+}
