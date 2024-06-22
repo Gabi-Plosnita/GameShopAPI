d5b3bacdd658a369a35bd44c54c4a785a5b8c84a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 21:10:33 +0300

    Implement IUserService

---
+++ GameShopAPI/GameShop.BusinessLogic/Services/UserService/UserService.cs
@@ -0,0 +1,100 @@
+﻿using GameShop.BusinessLogic.Mapping;
+using GameShop.DataAccess.Repositories;
+using GameShop.EntityLayer.Dtos;
+
+namespace GameShop.BusinessLogic.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAuthenticationService _authenticationService;
+
+        public UserService(IUserRepository userRepository, IAuthenticationService authenticationService)
+        {
+            _userRepository = userRepository;
+            _authenticationService = authenticationService;
+        }
+
+        public void Register(UserRequestDto userRequestDto)
+        {
+            try
+            {
+                string passwordHash = BCrypt.Net.BCrypt.HashPassword(userRequestDto.Password);
+                userRequestDto.Password = passwordHash;
+                var user = userRequestDto.MapToUser();
+                _userRepository.Register(user);
+
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public string Login(LoginRequestDto loginDto)
+        {
+            try
+            {
+                var user = _userRepository.GetByEmail(loginDto.Email);
+
+                if (!BCrypt.Net.BCrypt.Verify(loginDto.Password, user.Password))
+                {
+                    throw new Exception("Invalid password");
+                }
+
+                string token = _authenticationService.GenerateToken(user);
+                return token;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public List<UserResponseDto> GetAll()
+        {
+            var users = _userRepository.GetAll();
+            var userResponseDtos = users.MapToUserResponseDtos();
+            return userResponseDtos;
+        }
+
+        public UserResponseDto Get(int id)
+        {
+            try
+            {
+                var user = _userRepository.Get(id);
+                var userResponseDto = user.MapToUserResponseDto();
+                return userResponseDto;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public void Update(int id, UserRequestDto updatedUserDto)
+        {
+            try
+            {
+                var updatedUserEntity = updatedUserDto.MapToUser();
+                _userRepository.Update(id, updatedUserEntity);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                _userRepository.Delete(id);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
