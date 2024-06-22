5ae7b719f9ede87198870ec93bc434f6f158c9e8
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 15:42:41 +0300

    Create BaseRepository.cs

---
+++ GameShopAPI/GameShop.DataAccess/Repositories/BaseRepository/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using GameShop.DataAccess.DataContext;
+
+namespace GameShop.DataAccess.Repositories
+{
+    public class BaseRepository
+    {
+        private readonly GameShopDbContext _context;
+
+        public BaseRepository(GameShopDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
