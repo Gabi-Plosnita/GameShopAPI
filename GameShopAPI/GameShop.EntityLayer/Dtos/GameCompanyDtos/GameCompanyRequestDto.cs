0e3ea23127d7649663629120728ac9c4267a419c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-23 01:56:07 +0300

    Update GameCompanyRequestDto.cs

--- GameShopAPI/GameShop.EntityLayer/Dtos/GameCompanyDtos/GameCompanyRequestDto.cs
+++ GameShopAPI/GameShop.EntityLayer/Dtos/GameCompanyDtos/GameCompanyRequestDto.cs
@@ -1,8 +1,22 @@
-﻿namespace GameShop.EntityLayer.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.EntityLayer.Dtos
 {
-    public class GameCompanyRequestDto
+    public class GameCompanyRequestDto : IValidatableObject
     {
         public string Name { get; set; }
         public string Email { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required", new[] { nameof(Name) });
+            }
+            if (string.IsNullOrEmpty(Email)) 
+            {
+                yield return new ValidationResult("Email is required", new[] { nameof(Email) });
+            }
+        }
     }
 }
