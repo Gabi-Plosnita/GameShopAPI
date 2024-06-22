c16bef5cda22f1ef0b36ca22af94b87d4c7c14f0
m1toi <147705211+m1toi@users.noreply.github.com>
2024-06-22 20:48:28 +0300

    implemented all crud operations for user and role

--- GameShopAPI/GameShop.DataAccess/Repositories/RoleRepository/RoleRepository.cs
+++ GameShopAPI/GameShop.DataAccess/Repositories/RoleRepository/RoleRepository.cs
@@ -14,6 +14,16 @@ namespace GameShop.DataAccess.Repositories
             return _context.Roles.ToList();
         }
 
+        public Role Get(int id) 
+        {
+            var role = _context.Roles.Find(id);
+            if (role == null)
+            {
+                throw new Exception("Role not found");
+            }
+            return role;
+        }
+
         public void Create(Role role)
         {
             if (_context.Roles.Any(r => r.Name == role.Name))
@@ -21,6 +31,28 @@ namespace GameShop.DataAccess.Repositories
                 throw new Exception("Role already exists");
             }
             _context.Roles.Add(role);
+            SaveChanges();
+        }
+
+        public void Update(int id, Role updatedRole)
+        {
+            var roleToUpdate = _context.Roles.Find(id);
+            if (roleToUpdate == null)
+            {
+                throw new Exception("Role not found");
+            }
+            roleToUpdate.Name = updatedRole.Name;
+            SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var roleToDelete = _context.Roles.Find(id);
+            if (roleToDelete == null)
+            {
+                throw new Exception("Role not found");
+            }
+            _context.Roles.Remove(roleToDelete);
             SaveChanges();
         }
     }
