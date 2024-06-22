523ddf2f803d98431ef4bd3d41221af90ace060c
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 14:41:56 +0300

    Create GameShopDbContext.cs

---
+++ GameShopAPI/GameShop.DataAccess/DataContext/GameShopDbContext.cs
@@ -0,0 +1,33 @@
+﻿using GameShop.EntityLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameShop.DataAccess.DataContext
+{
+    public class GameShopDbContext : DbContext
+    {
+        public DbSet<Game> Games { get; set; }
+
+        public DbSet<GameCompany> GameCompanies { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public GameShopDbContext(DbContextOptions<GameShopDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Game //
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.GameCompany)
+                .WithMany(gc => gc.Games)
+                .HasForeignKey(g => g.GameCompanyId);
+
+            modelBuilder.Entity<Game>()
+                .HasOne(g => g.Category)
+                .WithMany(c => c.Games)
+                .HasForeignKey(g => g.CategoryId);
+
+        }
+    }
+}
