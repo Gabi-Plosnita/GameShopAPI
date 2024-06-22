33603db1a70c086071997b8ea9b4c9a5ae43f8f5
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-23 01:36:33 +0300

    Update CategoryRequestDto.cs

--- GameShopAPI/GameShop.EntityLayer/Dtos/CategoryDtos/CategoryRequestDto.cs
+++ GameShopAPI/GameShop.EntityLayer/Dtos/CategoryDtos/CategoryRequestDto.cs
@@ -1,7 +1,17 @@
-﻿namespace GameShop.EntityLayer.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.EntityLayer.Dtos
 {
-    public class CategoryRequestDto
+    public class CategoryRequestDto : IValidatableObject
     {
         public string Name { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required", new[] { nameof(Name) });
+            }
+        }
     }
 }
