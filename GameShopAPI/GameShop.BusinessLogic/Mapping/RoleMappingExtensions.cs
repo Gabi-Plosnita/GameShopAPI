23ddb167a2c7d995dd6782781198e279a4684e34
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 20:20:13 +0300

    Create mapping methods for Role and User

---
+++ GameShopAPI/GameShop.BusinessLogic/Mapping/RoleMappingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using GameShop.EntityLayer.Dtos;
+using GameShop.EntityLayer.Entities;
+
+namespace GameShop.BusinessLogic.Mapping
+{
+    public static class RoleMappingExtensions
+    {
+        public static RoleResponseDto MapToRoleResponseDto(this Role role)
+        {
+            return new RoleResponseDto
+            {
+                Id = role.RoleId,
+                Name = role.Name
+            };
+        }
+
+        public static List<RoleResponseDto> MapToRoleResponseDtos(this List<Role> roles)
+        {
+            return roles.Select(role => role.MapToRoleResponseDto()).ToList();
+        }
+
+        public static Role MapToRole(this RoleRequestDto roleRequestDto)
+        {
+            return new Role
+            {
+                Name = roleRequestDto.Name
+            };
+        }
+    }
+}
---
+++ GameShopAPI/GameShop.BusinessLogic/Mapping/UserMappingExtensions.cs
@@ -0,0 +1,35 @@
+﻿using GameShop.EntityLayer.Dtos;
+using GameShop.EntityLayer.Entities;
+
+namespace GameShop.BusinessLogic.Mapping
+{
+    public static class UserMappingExtensions
+    {
+        public static User MapToUser(this UserRequestDto userRequestDto)
+        {
+            return new User
+            {
+                Username = userRequestDto.Name,
+                Email = userRequestDto.Email,
+                Password = userRequestDto.Password,
+                RoleId = userRequestDto.RoleId
+            };
+        }
+
+        public static UserResponseDto MapToUserResponseDto(this User user)
+        {
+            return new UserResponseDto
+            {
+                Id = user.UserId,
+                Name = user.Username,
+                Email = user.Email,
+                Role = user.Role.Name
+            };
+        }
+
+        public static List<UserResponseDto> MapToUserResponseDtos(this List<User> users)
+        {
+            return users.Select(user => user.MapToUserResponseDto()).ToList();
+        }
+    }
+}
