73e19abc018cb38696acddf9bf1ec1c77ce767c1
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-23 01:29:15 +0300

    Update ServicesExtension.cs

--- GameShopAPI/GameShop.BusinessLogic/Extensions/ServicesExtension.cs
+++ GameShopAPI/GameShop.BusinessLogic/Extensions/ServicesExtension.cs
@@ -15,11 +15,16 @@ namespace GameShop.BusinessLogic.Extensions
             services.AddScoped<IGameRepository, GameRepository>();
             services.AddScoped<IGameCompanyRepository, GameCompanyRespository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
 
             // Configure Services
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
             services.AddScoped<IGameService, GameService>();
             services.AddScoped<IGameCompanyService, GameCompanyService>();
             services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<IUserService, UserService>();
         }
 
         public static void ConfigureDbContext(this IServiceCollection services, string connectionString)
