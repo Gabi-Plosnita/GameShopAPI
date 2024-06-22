c1c66e418326f293ba15a759d76997406f672cbd
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 20:19:47 +0300

    Update UserResponseDto.cs

--- GameShopAPI/GameShop.EntityLayer/Dtos/UserDtos/UserResponseDto.cs
+++ GameShopAPI/GameShop.EntityLayer/Dtos/UserDtos/UserResponseDto.cs
@@ -2,6 +2,9 @@
 {
     public class UserResponseDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
     }
 }
