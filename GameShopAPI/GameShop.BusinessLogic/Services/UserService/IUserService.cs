6f39ffac79ed3710f0b92bf12a86c3136ecf76fd
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 20:45:23 +0300

    Create IUserService.cs

---
+++ GameShopAPI/GameShop.BusinessLogic/Services/UserService/IUserService.cs
@@ -0,0 +1,19 @@
+﻿using GameShop.EntityLayer.Dtos;
+
+namespace GameShop.BusinessLogic.Services.UserService
+{
+    public interface IUserService
+    {
+        void Register(UserRequestDto userRequestDto);
+
+        string Login(LoginRequestDto loginDto);
+
+        List<UserResponseDto> GetAll();
+
+        UserResponseDto Get(int id);
+
+        void Update(int id, UserRequestDto updatedUserDto);
+
+        void Delete(int id);
+    }
+}
