f80ad5ef3a3e5dfe9a6e86cbb606f4efcc4b90ae
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-23 02:04:41 +0300

    Update LoginRequestDto.cs

--- GameShopAPI/GameShop.EntityLayer/Dtos/LoginDtos/LoginRequestDto.cs
+++ GameShopAPI/GameShop.EntityLayer/Dtos/LoginDtos/LoginRequestDto.cs
@@ -1,8 +1,22 @@
-﻿namespace GameShop.EntityLayer.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.EntityLayer.Dtos
 {
-    public class LoginRequestDto
+    public class LoginRequestDto : IValidatableObject
     {
         public string Email { get; set; }
         public string Password { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(string.IsNullOrEmpty(Email))
+            {
+                yield return new ValidationResult("Email is required", new[] { nameof(Email) });
+            }
+            if(string.IsNullOrEmpty(Password)) 
+            {
+                yield return new ValidationResult("Password is required", new[] { nameof(Password) });
+            }
+        }
     }
 }
