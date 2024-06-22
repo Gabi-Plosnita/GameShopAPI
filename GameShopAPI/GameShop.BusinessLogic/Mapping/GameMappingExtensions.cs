01b679c3170a4295160d13744f5a8719eb0b30a9
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 17:03:08 +0300

    Create GameMappingExtensions.cs

---
+++ GameShopAPI/GameShop.BusinessLogic/Mapping/GameMappingExtensions.cs
@@ -0,0 +1,43 @@
+﻿using GameShop.EntityLayer.Dtos;
+using GameShop.EntityLayer.Entities;
+
+namespace GameShop.BusinessLogic.Mapping
+{
+    public static class GameMappingExtensions
+    {
+        public static GameResponseDto ToGameResponseDto(this Game game)
+        {
+            var result = new GameResponseDto
+            {
+                GameId = game.GameId,
+                Name = game.Name,
+                Price = game.Price,
+                GameCompanyName = game.GameCompany.Name,
+                CategoryName = game.Category.Name
+            };
+            return result;
+        }
+
+        public static Game ToGame(this GameRequestDto gameRequestDto)
+        {
+            var result = new Game
+            {
+                Name = gameRequestDto.Name,
+                Price = gameRequestDto.Price,
+                GameCompanyId = gameRequestDto.GameCompanyId,
+                CategoryId = gameRequestDto.CategoryId
+            };
+            return result;
+        }
+
+        public static List<GameResponseDto> ToGameResponseDtos(this List<Game> games)
+        {
+            var result = new List<GameResponseDto>();
+            foreach (var game in games)
+            {
+                result.Add(game.ToGameResponseDto());
+            }
+            return result;
+        }
+    }
+}
