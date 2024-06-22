0217515f4a7a580f0bafff6764d140a048d10359
m1toi <147705211+m1toi@users.noreply.github.com>
2024-06-23 00:13:20 +0300

    create new exception

---
+++ GameShopAPI/GameShop.EntityLayer/Exceptions/UserExceptions/InvalidPasswordException.cs
@@ -0,0 +1,14 @@
+﻿namespace GameShop.EntityLayer.Exceptions
+{
+    public class InvalidPasswordException : Exception
+    {
+        public InvalidPasswordException()
+        {
+        }
+
+        public InvalidPasswordException(string message)
+            : base(message)
+        {
+        }
+    }
+}
