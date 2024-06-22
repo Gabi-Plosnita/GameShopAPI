bae426c7b46257ca19f466d900ffd7af1e08512a
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-23 02:04:46 +0300

    Update UserRequestDto.cs

--- GameShopAPI/GameShop.EntityLayer/Dtos/UserDtos/UserRequestDto.cs
+++ GameShopAPI/GameShop.EntityLayer/Dtos/UserDtos/UserRequestDto.cs
@@ -1,10 +1,39 @@
-﻿namespace GameShop.EntityLayer.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.EntityLayer.Dtos
 {
-    public class UserRequestDto
+    public class UserRequestDto : IValidatableObject
     {
         public string Name { get; set; }
+
         public string Email { get; set; }
+
         public string Password { get; set; }
-        public int RoleId { get; set; }
+
+        public int RoleId { get; set; }    
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required", new[] { nameof(Name) });
+            }
+
+            if (string.IsNullOrEmpty(Email))
+            {
+                yield return new ValidationResult("Email is required", new[] { nameof(Email) });
+            }
+
+            if (string.IsNullOrEmpty(Password))
+            {
+                yield return new ValidationResult("Password is required", new[] { nameof(Password) });
+            }
+
+            if(RoleId <= 0)
+            {
+                yield return new ValidationResult("RoleId must be greater than 0", new[] { nameof(RoleId) });
+            }
+        }
+
     }
 }
