9014d7d853f85a295e4969673976515957ea0664
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 20:25:06 +0300

    Create RoleService.cs

---
+++ GameShopAPI/GameShop.BusinessLogic/Services/RoleService/RoleService.cs
@@ -0,0 +1,29 @@
+﻿using GameShop.BusinessLogic.Mapping;
+using GameShop.DataAccess.Repositories;
+using GameShop.EntityLayer.Dtos;
+
+namespace GameShop.BusinessLogic.Services.RoleService
+{
+    public class RoleService : IRoleService
+    {
+        private readonly IRoleRepository _roleRepository;
+
+        public RoleService(IRoleRepository roleRepository)
+        {
+            _roleRepository = roleRepository;
+        }
+
+        public List<RoleResponseDto> GetAll()
+        {
+            var roles = _roleRepository.GetAll();
+            var rolesDtos = roles.MapToRoleResponseDtos();
+            return rolesDtos;
+        }
+
+        public void Create(RoleRequestDto roleDto)
+        {
+            var role = roleDto.MapToRole();
+            _roleRepository.Create(role);
+        }
+    }
+}
