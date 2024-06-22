ffd766938c31d1202daf142f2ba1249b14ae673b
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 18:53:34 +0300

    Create GamesController.cs

---
+++ GameShopAPI/GameShopAPI/Controllers/GamesController.cs
@@ -0,0 +1,57 @@
+﻿using GameShop.BusinessLogic.Services;
+using GameShop.EntityLayer.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GameShop.API.Controllers
+{
+    [Route("api/Games")]
+    public class GamesController : ControllerBase
+    {
+        private readonly IGameService _gameService;
+
+        public GamesController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public List<GameResponseDto> GetAll()
+        {
+            return _gameService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public GameResponseDto GetById([FromRoute] int id)
+        {
+           return _gameService.GetById(id);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public void Create([FromBody] GameRequestDto gameDto)
+        {
+            _gameService.Create(gameDto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public void Update([FromRoute] int id, [FromBody] GameRequestDto updatedGameDto)
+        {
+            _gameService.Update(id, updatedGameDto);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public void Delete([FromRoute] int id)
+        {
+            _gameService.Delete(id);
+        }
+    }
+}
