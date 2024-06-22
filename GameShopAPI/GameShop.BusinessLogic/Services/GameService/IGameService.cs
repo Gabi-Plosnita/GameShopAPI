5cbf7ac235ed25aa615c669221d192d114599096
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-22 17:28:48 +0300

    Update IGameService.cs

--- GameShopAPI/GameShop.BusinessLogic/Services/GameService/IGameService.cs
+++ GameShopAPI/GameShop.BusinessLogic/Services/GameService/IGameService.cs
@@ -1,6 +1,17 @@
-﻿namespace GameShop.BusinessLogic.Services
+﻿using GameShop.EntityLayer.Dtos;
+
+namespace GameShop.BusinessLogic.Services
 {
     public interface IGameService
     {
+        List<GameResponseDto> GetAll();
+
+        GameResponseDto GetById(int id);
+
+        void Add(GameRequestDto game);
+
+        void Update(int id, GameRequestDto updatedGame);
+
+        void Delete(int id);
     }
 }
