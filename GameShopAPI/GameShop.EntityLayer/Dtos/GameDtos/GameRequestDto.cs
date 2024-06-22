afc3a7d7167cb7eaee23a1cf972176e4bb79cd3b
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2024-06-23 02:00:32 +0300

    Update GameRequestDto.cs

--- GameShopAPI/GameShop.EntityLayer/Dtos/GameDtos/GameRequestDto.cs
+++ GameShopAPI/GameShop.EntityLayer/Dtos/GameDtos/GameRequestDto.cs
@@ -1,6 +1,8 @@
-﻿namespace GameShop.EntityLayer.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameShop.EntityLayer.Dtos
 {
-    public class GameRequestDto
+    public class GameRequestDto : IValidatableObject
     {
         public string Name { get; set; }
 
@@ -9,5 +11,25 @@
         public int GameCompanyId { get; set; }
 
         public int CategoryId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Name))
+            {
+                yield return new ValidationResult("Name is required", new[] { nameof(Name) });
+            }
+            if(Price <= 0)
+            {
+                yield return new ValidationResult("Price must be greater than 0", new[] { nameof(Price) });
+            }
+            if(GameCompanyId <= 0)
+            {
+                yield return new ValidationResult("GameCompanyId must be greater than 0", new[] { nameof(GameCompanyId) });
+            }
+            if(CategoryId <= 0)
+            {
+                yield return new ValidationResult("CategoryId must be greater than 0", new[] { nameof(CategoryId) });
+            }
+        }
     }
 }
