6ffe854435d52c21a9fe8b74ec6b74077e4aaaaa
m1toi <147705211+m1toi@users.noreply.github.com>
2024-06-23 13:19:15 +0300

    Update UserController.cs

--- GameShopAPI/GameShopAPI/Controllers/UserController.cs
+++ GameShopAPI/GameShopAPI/Controllers/UserController.cs
@@ -16,9 +16,16 @@ namespace GameShop.API.Controllers
             _userService = userService;
         }
 
+        
+        //Returns all users
+        //Anyone can access this endpoint
+        //200Ok is returned if the request is successful
+        //400BadRequest is returned if the request is invalid
+        //404NotFound is returned if the user is not found or role is not found
+        //409Conflict is returned if the user already exists
         [AllowAnonymous]
         [HttpPost("/register")]
-        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status409Conflict)]
@@ -31,9 +38,15 @@ namespace GameShop.API.Controllers
             }
 
             _userService.Register(user);
-            return Created();
+            return Ok();
         }
 
+
+        //Logs in a user
+        //Anyone can access this endpoint
+        //200Ok is returned if the request is successful
+        //400BadRequest is returned if the request is invalid
+        //404NotFound is returned if the user is not found
         [AllowAnonymous]
         [HttpPost("/login")]
         [ProducesResponseType(StatusCodes.Status200OK)]
@@ -51,6 +64,11 @@ namespace GameShop.API.Controllers
             return Ok(token);
         }
 
+
+        //Returns all users
+        //Only admins can access this endpoint
+        //200Ok is returned if the request is successful
+        //401Unauthorized is returned if the user is not authorized
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -60,6 +78,12 @@ namespace GameShop.API.Controllers
             return Ok(_userService.GetAll());
         }
 
+
+        //Returns a user by id
+        //Only admins can access this endpoint
+        //200Ok is returned if the request is successful
+        //401Unauthorized is returned if the user is not authorized
+        //404NotFound is returned if the user is not found
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
@@ -70,6 +94,14 @@ namespace GameShop.API.Controllers
             return Ok(_userService.Get(id));
         }
 
+
+        //Updates a user by id
+        //Only admins can access this endpoint
+        //204NoContent is returned if the request is successful
+        //400BadRequest is returned if the request is invalid
+        //401Unauthorized is returned if the user is not authorized
+        //404NotFound is returned if the user is not found or role is not found
+        //409Conflict is returned if the user already exists
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -88,6 +120,12 @@ namespace GameShop.API.Controllers
             return NoContent();
         }
 
+
+        //Deletes a user by id
+        //Only admins can access this endpoint
+        //204NoContent is returned if the request is successful
+        //401Unauthorized is returned if the user is not authorized
+        //404NotFound is returned if the user is not found
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
